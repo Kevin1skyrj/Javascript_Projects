347cd9b4ede15df4840fe0e9c3673eeed4d6b924
Rajat Pandey <rajatpndey257@gmail.com>
2026-01-13 07:58:11 +0530

    counter app done

---
+++ Counter-App/index.js
@@ -0,0 +1,19 @@
+const increment = document.getElementById('increment')
+const decrement = document.getElementById('decrement')
+
+const counterValue = document.getElementById('counter-value');
+
+let count = 0;
+//increment 
+increment.addEventListener('click' , function(){
+    count++;
+    counterValue.textContent = count;
+    console.log(count)
+})
+decrement.addEventListener('click' , function(){
+    count--;
+    counterValue.textContent = count;
+    console.log(count)
+})
+
+console.log(count);
