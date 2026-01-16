036630bd036a1b13089b8c27326863d3597766ce
Rajat Pandey <rajatpndey257@gmail.com>
2026-01-16 10:29:00 +0530

    work in progress

--- Bill-splitter/script.js
+++ Bill-splitter/script.js
@@ -1,26 +1,35 @@
 const tipContainer = document.querySelector('.tip-container');
-const billAmount = document.getElementById('bill-amount');
-const customTip = document.querySelector('.custom-tip');
-const numberOfPeople = document.querySelector('.number-of-people');
+const billAmountInput = document.getElementById('bill-amount');
+const customTipInput = document.querySelector('.custom-tip');
+const numberOfPeopleInput = document.querySelector('.number-of-people');
 const generateBill = document.querySelector('.generate-bill-btn');
+const tipAmountOutput = document.querySelector('.tip-amount span');
+const toalAmountOutput = document.querySelector('.total span');
+const eachPersonBill = document.querySelector('.each-person-bill span');
+const reset = document.querySelector('.reset-btn');
 
 
-billAmount.addEventListener('input', () =>{
-  console.log('You are typing')
+billAmountInput.addEventListener('input', () =>{
+  if(billAmountInput.value){
+    customTipInput.disabled = false;
+    numberOfPeopleInput.disabled = false;
+    tipContainer.classList.remove('disabled');
+  }
 })
 
 tipContainer.addEventListener('click', () => {
    console.log("clicked")
 })
 
-customTip.addEventListener('input',() =>{
+customTipInput.addEventListener('input',() =>{
     console.log('custom type')
 })
 
-numberOfPeople.addEventListener('input',() =>{
+numberOfPeopleInput.addEventListener('input',() =>{
     console.log('People typing')
 })
 
 generateBill.addEventListener('click',() =>{
+    
     console.log('GenerateBill clicked')
 })
