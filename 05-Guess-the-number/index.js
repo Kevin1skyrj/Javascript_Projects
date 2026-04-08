6dfddaeab686608aac653aa7317279ca06fae042
Rajat Pandey <rajatpndey257@gmail.com>
2026-04-08 22:12:06 +0530

    almost guess the number game completed

---
+++ 05-Guess-the-number/index.js
@@ -0,0 +1,31 @@
+let randomNumber = Math.round(Math.random()*100);
+// console.log(randomNumber);
+const userInput = document.querySelector('.form-input');
+const form = document.querySelector('form');
+const result = document.querySelector('.result');
+const allGuesses = document.querySelector('.all-guesses');
+const submitBtn = document.querySelector('.btn-primary');
+const newGameBtn = document.querySelector('.btn-secondary');
+
+const allGuessesArray = [];
+
+form.addEventListener('submit', (e) => {
+       e.preventDefault();
+       const userInputValue = parseInt(userInput.value);
+       if(userInputValue < randomNumber){
+        result.innerText = `${userInputValue} is Too Low! `
+       }else if(userInputValue > randomNumber){
+        result.innerText = `${userInputValue} is Too High! `
+       }else{
+        result.innerText = 'Congrats🎉 Your guess is correct'
+        newGameBtn.disabled = false;
+        submitBtn.disabled = true;
+       }
+       allGuessesArray.push(userInputValue);
+       allGuesses.innerText = allGuessesArray.join(', ')
+       form.reset();
+})
+
+newGameBtn.addEventListener('click', () =>{
+    
+})
