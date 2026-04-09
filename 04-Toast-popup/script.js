da7348a325d4178f42bd41e3a9300794d1a37e66
Rajat Pandey <rajatpndey257@gmail.com>
2026-04-09 10:03:12 +0530

    working on toast-popup project

--- 04-Toast-popup/script.js
+++ 04-Toast-popup/script.js
@@ -4,17 +4,20 @@ const toastType = document.querySelector("#toast-type");
 const toastMessage = document.querySelector("#toast-message");
 const durationContainer = document.querySelector("#duration-container");
 const showToastBtn = document.querySelector(".show-toast");
-const toastContainer = document.querySelector(".toast-container");
+const toastContainer = document.querySelector(".toasts-container");
 
 showToastBtn.addEventListener("click", () => {
-  if (horizontalPosition.ariaValueMax === "right") {
+  const newToast = document.createElement('div');
+  newToast.classList.add('toast');
+
+  if (horizontalPosition.value === "right") {
     toastContainer.classList.add("right");
   } else {
-    // toastContainer.classList.remove("right");
+    toastContainer.classList.remove("right");
   }
-  if (verticalPosition.ariaValueMax === "bottom") {
+  if (verticalPosition.value === "bottom") {
     toastContainer.classList.add("bottom");
   } else {
-    // toastContainer.classList.remove("bottom");
+    toastContainer.classList.remove("bottom");
   }
 });
